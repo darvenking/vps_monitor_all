1c0bcc9aec0c76086434450067894566fb4736e6
lalala <julong2@gmail.com>
2022-11-10 21:11:17 +0800

    代码修改

--- vps_monitor_client/main.go
+++ vps_monitor_client/main.go
@@ -32,7 +32,7 @@ func handle(siteInfo *db.SiteInfo) {
 		if siteInfo.Stock != b {
 			db.GetSiteInfoDB().Where("id = ?", siteInfo.ID).Update("stock", b)
 		}
-		fmt.Sprintf("%s读取完成:%s", time.Now().Format("2006-01-02"), siteInfo.URL)
+		fmt.Printf("%s更新完成:%s", time.Now().Format("2006-01-02 15:04:05"), siteInfo.URL)
 		time.Sleep(30 * time.Second)
 	}
 
